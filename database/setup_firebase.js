4cc3d3f5b07e7c1104787f6f99b149d55287470a
Esuom1 <yerdna.voleyrog@gmail.com>
2019-03-13 02:32:00 -0700

    updated folders

---
+++ database/setup_firebase.js
@@ -0,0 +1,11 @@
+(function() {
+  var config = {
+    apiKey: "AIzaSyBgIUMTcKedvx5m0w3yPoUtYUJ7XmVTwAY",
+    authDomain: "foodkeep.firebaseapp.com",
+    databaseURL: "https://foodkeep.firebaseio.com",
+    projectId: "foodkeep",
+    storageBucket: "foodkeep.appspot.com",
+    messagingSenderId: "1020242428178"
+  };
+  firebase.initializeApp(config);
+})();
