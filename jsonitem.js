d2397a2d178286e35fb6bc88e3b310a8a3283f55
Young <youngkwon313@gmail.com>
2019-03-13 09:54:18 -0700

    json item change

--- jsonitem.js
+++ jsonitem.js
@@ -1,16 +1,16 @@
 
-function createItem(addName){
+function createItem(addName, exnum){
   var today = new Date();
   var dd = today.getDate();
   var mm = today.getMonth();
   var year = today.getFullYear();
-  var exday = dd + 14;
+  var exday = dd + exnum;
   var myItem = {
     "name": addName,
     "number": 1,
-    "date": dd + '/' + mm + '/' + year,
+    "date": year + mm + '-' + '-' + dd,
     "added": true,
-    "expdate": exday + '/' + mm + '/' + year,
+    "expdate": year + mm + '-' + '-' + exday,
     "expired": false,
     "deleted": false
   }
@@ -35,4 +35,16 @@ function hideCategories(){
   var openButton = document.createElement("div");
   openButton.setAttribute("id", "openIcon");
   openButton.innerHTML = "<img src = \"Images/add_button.png\" alt = \"add\">";
+}
+
+function calcExpDays(myItem){
+  var today = new Date();
+  var expDay = new Date(myItem.expdate);
+  var diff = new Date(expDay - today);
+  var millsec = 1000;
+  var seconds = 60;
+  var minutes = 60;
+  var hours = 24;
+  var numdays = Math.ceil(diff/millsec/seconds/minutes/hours);
+  return numdays;
 }
